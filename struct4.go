531bb71b75c2f02bc1d6939527ad6eb200a1e481
Joao-azzolin <126572160+Joao-azzolin@users.noreply.github.com>
2023-06-13 11:21:33 -0300

    Create struct4.go

---
+++ struct4.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Musica struct {
+	Titulo  string
+	Artista string
+	Duracao time.Duration
+}
+
+type Playlist struct {
+	Nome    string
+	Musicas []Musica
+}
+
+func (p Playlist) DuracaoTotal() time.Duration {
+	total := time.Duration(0)
+	for _, m := range p.Musicas {
+		total += m.Duracao
+	}
+	return total
+}
+
+func imprimePlaylist(p Playlist) {
+	fmt.Println(p.Nome)
+	fmt.Println(p.DuracaoTotal())
+	for _, m := range p.Musicas {
+		fmt.Println(m.Titulo, m.Artista, m.Duracao)
+	}
+}
+
+func main() {
+	playlist := Playlist{
+		Nome: "Forró Raiz",
+		Musicas: []Musica{
+			{Titulo: "Isso é Vaquejada", Artista: "Tarcísio do Acordeon", Duracao: time.Second * 190},
+			{Titulo: "Nunca Mais", Artista: "NATTAN e Xand Avião", Duracao: time.Second * 187},
+			{Titulo: "Americana na Vaquejada", Artista: "Grandão Vaqueiro e NATTAN", Duracao: time.Second * 156},
+		},
+	}
+
+	imprimePlaylist(playlist)
+}
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Musica struct {
+	Titulo  string
+	Artista string
+	Duracao time.Duration
+}
+
+type Playlist struct {
+	Nome    string
+	Musicas []Musica
+}
+
+func (p Playlist) DuracaoTotal() time.Duration {
+	total := time.Duration(0)
+	for _, m := range p.Musicas {
+		total += m.Duracao
+	}
+	return total
+}
+
+func imprimePlaylist(p Playlist) {
+	fmt.Println(p.Nome)
+	fmt.Println(p.DuracaoTotal())
+	for _, m := range p.Musicas {
+		fmt.Println(m.Titulo, m.Artista, m.Duracao)
+	}
+}
+
+func main() {
+	playlist := Playlist{
+		Nome: "Forró Raiz",
+		Musicas: []Musica{
+			{Titulo: "Isso é Vaquejada", Artista: "Tarcísio do Acordeon", Duracao: time.Second * 190},
+			{Titulo: "Nunca Mais", Artista: "NATTAN e Xand Avião", Duracao: time.Second * 187},
+			{Titulo: "Americana na Vaquejada", Artista: "Grandão Vaqueiro e NATTAN", Duracao: time.Second * 156},
+		},
+	}
+
+	imprimePlaylist(playlist)
+}
